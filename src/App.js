3eab751da2cc8266409620f9f712541c7ed136b9
unknown <umar.musya.12@gmail.com>
2024-06-01 17:27:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Header from "./components/header"
+import Body from "./components/body"
+import Footer from "./components/footer"
+import './components/index.css'
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
